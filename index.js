daa26872f9487122a596200ffcb2e733d5a4110f
takinabradley <bradfinney98@gmail.com>
2023-03-20 02:42:45 -0400

    clean up code

--- index.js
+++ index.js
@@ -7,24 +7,6 @@ async function openDevDocsInNewTab() {
   return devDocsTab
 }
 
-/* function getSearchFunc(query) {
-  return `\
-    console.log("${query}");\
-    const searchBar = document.querySelector("._search-input");\
-    const formElem = searchBar.parentElement;\
-    searchBar.value = "${query}";\
-    console.log(searchBar.value);\
-    formElem.submit();\
-  `
-}
-
-async function searchDevDocs(tab, query) {
-  // run a script in that tab
-  browser.tabs.executeScript(tab.id, {
-    code: getSearchFunc(query)
-  })
-} */
-
 function closeExtension() {
   browser.runtime.sendMessage({ type: "close-extension" })
 }
@@ -44,6 +26,5 @@ document.addEventListener("submit", async (e) => {
   e.preventDefault()
   const devDocsTab = await openDevDocsInNewTab()
   sendQueryToTabScript(devDocsTab, input.value)
-  /* searchDevDocs(devDocsTab, input.value) */
   closeExtension()
 })
--- listenForSearch.js
+++ listenForSearch.js
@@ -1,8 +1,10 @@
-browser.runtime.onMessage.addListener((message) => {
+function searchDevDocs(message) {
   if (message.type === "search-query") {
     const searchBar = document.querySelector("._search-input")
     const formElem = searchBar.parentElement
     searchBar.value = message.data
     formElem.submit()
   }
-})
+}
+
+browser.runtime.onMessage.addListener(searchDevDocs)
